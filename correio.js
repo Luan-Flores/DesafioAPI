8627bdd265c3eb5ef6d2dda82e41b4f97198bc71
Luan-Flores <keeganluan919@gmail.com>
2025-02-07 09:22:57 -0400

    Programa feito

--- correio.js
+++ correio.js
@@ -3,51 +3,80 @@ const log = document.getElementById('logradouro');
 const bairro = document.getElementById('bairro');
 const localidade = document.getElementById('localidade');
 const uf = document.getElementById('uf');
+const pErro = document.getElementById('pErro');
+const divErro = document.querySelector('.alert');
+const imgErro = document.getElementById('imgErro');
 
+function resetAlert(){
+    pErro.innerText = "";
+    imgErro.src = "";
+}
+function resetResult(){
+    log.innerText = `Logradouro: `
+    bairro.innerText = `Bairro: `
+    localidade.innerText = "Localidade: "
+    uf.innerText = `UF: `
+}
 document.getElementById("btnBuscar").addEventListener('click', async function() {
     const cep = document.getElementById("inputCep").value.trim(); // Pega o valor do input após o clique
     if (!cep) {
         console.error("Por favor, digite um CEP.");
+        divErro.style.display = "flex";
+        pErro.innerText = `Digite o CEP. `;
+        imgErro.src = "./imgs/warning.png";
+        resetResult();
         return;
     }
-
+    
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-
+    
     try {
         const response = await fetch(url);
         
         if (!response.ok) {
+            divErro.style.display = "flex";
+            pErro.innerText = `Erro no servidor`;
+            imgErro.src = "./imgs/warning.png";
+            resetResult();
             throw new Error(`Erro: ${response.status}`);
         }
-
+        
         const data = await response.json();
-
+        
         if (data.erro) {
+            divErro.style.display = "flex";
             console.error("CEP não encontrado.");
+            pErro.innerText = `CEP não encontrado. `;
+            imgErro.src = "./imgs/warning.png";
+            resetResult();
         } else {
             console.log(data);
-
-            log.innerText += ` ${data.logradouro}`
-            bairro.innerText += ` ${data.bairro}`
-            localidade.innerText += ` ${data.localidade}`
-            uf.innerText += ` ${data.uf}`
-
+            resetAlert();
             log.innerText = `Logradouro: ${data.logradouro}`
             bairro.innerText = `Bairro: ${data.bairro}`
             localidade.innerText = `Localidade: ${data.localidade}`
             uf.innerText = `UF: ${data.uf}`
-
         }
     } catch (error) {
         console.error("Erro ao buscar o CEP:", error);
+        resetResult();
+        divErro.style.display = "flex";
+        pErro.innerText = "Erro ao buscar o CEP. ";
+        imgErro.src = "./imgs/warning.png";
     }
 });
 
 //! ↓ copar as li (logradouro, bairro, localizade, uf) pra area de transferencia ↓
 
-document.querySelector(".imgCopy").addEventListener("click", function(){
-    const eliIs = document.querySelectorAll(".ulRes .liRes");
-    const elisTransformadosEmStrings = Array.from(eliIs).map(item => item.innerText);
-    const copy = elisTransformadosEmStrings.join("\n");
-    navigator.clipboard.writeText(copy)
-});
+    document.querySelector(".imgCopy").addEventListener("click", function(){
+        if(log.innerText === `Logradouro: ` & bairro.innerText === `Bairro: ` & localidade.innerText === `Localidade: ` & uf.innerText === `UF: `){
+            alert("ALas");
+            return;
+        }else{   
+            const eliIs = document.querySelectorAll(".ulRes .liRes");
+            const elisTransformadosEmStrings = Array.from(eliIs).map(item => item.innerText);
+            const copy = elisTransformadosEmStrings.join("\n");
+            navigator.clipboard.writeText(copy);
+        }
+    })
+
